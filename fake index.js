8a8391b249b4cd5d417b2807ed0869107154f41e
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-05-18 18:13:51 -0400

    commit tree 88dadb66b0beb434674d88ec4a3f0ce109b0ccac

---
+++ fake index.js
@@ -0,0 +1,31 @@
+// function saturdayFun(hobby ="roller-skate") {
+//     return `On Saturday, I like to ${hobby}`
+// }
+// saturdayFun();
+// saturdayFun("eat icecream");
+
+// function mondayWork(work = "go to office") {
+//     return `I start my week taking the train to ${mondayWork}`
+// }
+// mondayWork();
+// mondayWork("the park");
+
+// //OR
+
+// const work = function (mondayWork="go to office") {
+//     return `I start my week taking the train to ${mondayWork}`
+// };
+
+// const job = function (travel ="in plane") {
+//     return `My job has me ${travel} almost every 2 weeks`
+// }
+// // console.log(job("on the road"));
+
+// function wrapAdjective(emphasis = "*") {
+//     const innerFunction = function (partOne = "special"){
+//         return `You are ${emphasis} ${partOne} ${emphasis}`
+//     };
+//     return innerFunction;
+// }
+
+// console.log(wrapAdjective("!!")("to me"));
